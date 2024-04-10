1fadece63001092769c1991ecfa412901fa16ff9
marcin-cto <132883228+marcin-cto@users.noreply.github.com>
2024-04-10 10:30:26 +0200

    Create helm.tf

---
+++ helm.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "nginx" {
+  name        = "beta-nginx"
+  repository  = "https://kubernetes.github.io/ingress-nginx"
+  chart       = "ingress-nginx"
+  namespace   = "default"
+  set {
+    name  = "controller.service.type"
+    value = "NodePort"
+  }
+}
+
+data "kubernetes_service" "nginx" {
+  depends_on = [helm_release.nginx]
+  metadata {
+    name = "beta-nginx-ingress-nginx-controller"
+  }
+}
+
+output "nginx_endpoint" {
+  value = data.kubernetes_service.nginx
+}
+
+# variable "replica_count" {
+#   type = string
+#   description = "How many Nginx pods do you wish to run"
+# }
